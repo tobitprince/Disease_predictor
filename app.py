db605c2cda8cd0ffbe219876386c4f43d5127451
tobitprince <prince.tobit@strathmore.edu>
2023-11-27 19:15:25 +0300

    model

--- app.py
+++ app.py
@@ -33,6 +33,9 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from itsdangerous import URLSafeTimedSerializer
 from werkzeug.security import check_password_hash
+from PIL import Image
+import torchvision.transforms.functional as TF
+import CNN
 
 
 app = Flask(__name__)
@@ -142,6 +145,43 @@ def predict_image(img, model=disease_model):
     # Retrieve the class label
     return prediction
 
+
+
+disease_info = pd.read_csv('disease_info.csv' , encoding='cp1252')
+supplement_info = pd.read_csv('supplement_info.csv',encoding='cp1252')
+
+model = CNN.CNN(39)    
+model.load_state_dict(torch.load("plant_disease_model_1_latest.pt"))
+model.eval()
+
+def prediction(image_path):
+    image = Image.open(image_path)
+    image = image.resize((224, 224))
+    input_data = TF.to_tensor(image)
+    input_data = input_data.view((-1, 3, 224, 224))
+    output = model(input_data)
+    output = output.detach().numpy()
+    index = np.argmax(output)
+    return index
+
+@app.route('/submit', methods=['GET', 'POST'])
+def submit():
+    if request.method == 'POST':
+        image = request.files['image']
+        filename = image.filename
+        file_path = os.path.join('static/uploads', filename)
+        image.save(file_path)
+        print(file_path)
+        pred = prediction(file_path)
+        title = disease_info['disease_name'][pred]
+        description =disease_info['description'][pred]
+        prevent = disease_info['Possible Steps'][pred]
+        image_url = disease_info['image_url'][pred]
+        supplement_name = supplement_info['supplement name'][pred]
+        supplement_image_url = supplement_info['supplement image'][pred]
+        supplement_buy_link = supplement_info['buy link'][pred]
+        return render_template('submit.html',username=session['username'] , title = title , desc = description , prevent = prevent , 
+                               image_url = image_url , pred = pred ,sname = supplement_name , simage = supplement_image_url , buy_link = supplement_buy_link)
 
 
 @ app.route('/')
